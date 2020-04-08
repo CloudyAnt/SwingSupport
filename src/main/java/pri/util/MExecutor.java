137cc2d2a98a28d58d1ef6c332510c850d30b013
itscloudy <itscloudy@foxmail.com>
2020-04-08 08:21:27 +0800

    [chai] refactor: add shutdown method to MExecutor

--- src/main/java/pri/util/MExecutor.java
+++ src/main/java/pri/util/MExecutor.java
@@ -6,8 +6,15 @@ import java.util.concurrent.Executors;
 public class MExecutor {
     private MExecutor() {}
 
-    private static ExecutorService service = Executors.newCachedThreadPool();
+    private static ExecutorService service;
     public static void execute(Runnable task) {
+        if (service == null || service.isShutdown()) {
+            service = Executors.newCachedThreadPool();
+        }
         service.execute(task);
+    }
+
+    public static void shutdown() {
+        service.shutdown();
     }
 }
